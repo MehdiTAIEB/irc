d44fe016171affdc8ca1da3c6610bfe2ee25c69b
Mehdi <mehdi.taieb@epitech.eu>
2016-01-24 19:50:41 +0100

    annotation

--- public/js/client.js
+++ public/js/client.js
@@ -21,6 +21,9 @@ object chan
 // if not selected tab get a new message light it ?
 // on the left navbarstyle
 
-
+// same page so if isset main name hide all login form else hide other// rename login by index.html
 
 // emit message with parameter that id user and feed then broadcast it to the chan
+//
+
+// add angular to client without route system just to make dom interaction easier
