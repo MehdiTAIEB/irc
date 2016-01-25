298a950dfac9957580ee984ee29e033dafc3a953
Mehdi <mehdi.taieb@epitech.eu>
2016-01-25 17:20:41 +0100

    indent html

--- public/js/dash/dash.controller.js
+++ public/js/dash/dash.controller.js
@@ -8,10 +8,11 @@
 		function DashController($location, socket) {
 			var vm = this;
 			vm.socket = socket;
+
 			vm.socket.emit('getId', {});
 			vm.socket.on('id', function (data) {
 				vm.mainName = data.id;
 			});
-			$(".chat").niceScroll();
+			$(".chat").niceScroll();// # sale
 		}
 })();
